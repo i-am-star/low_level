b25fe772e9a40a06d584c94a7b44899f098239c9
i-am-star <adannaya649@gmail.com>
2023-03-01 14:59:40 +0100

    Learning C language at ALX is fungit add .!

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+*main - Using printf to print a line of test
+*
+*Return: 0
+*/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+*main - A program that prints the size of data types
+*
+*Return: 0
+*/
+int main(void)
+{
+	//char a;
+	//int num;
+	//long int;
+	//long long int;
+	//float;
+	printf("size of char: %d bytes(s)\n", sizeof(char));
+	return (0);
+
+}
